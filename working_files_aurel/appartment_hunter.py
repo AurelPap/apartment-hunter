e1d201d662520c0ef5089588e7b6ee14f105314d
AurelPap <apapillon@aurelpap.fr>
2024-03-28 11:07:16 +0100

    feat: Analyse univariée des variables quantitatives

---
+++ working_files_aurel/appartment_hunter.py
@@ -0,0 +1,46 @@
+# Nomination des variables qualitatives et quantitatives
+variables_quantitatives = [
+    "price",
+    "bedrooms",
+    "bathrooms",
+    "sqft_living",
+    "sqft_lot",
+    "floors",
+    "sqft_above",
+    "sqft_basement",
+    "yr_built",
+    "yr_renovated",
+    "sqft_living15",
+    "sqft_lot15"
+]
+
+colors_quantitative = [
+    '#FF6F61',  # Rouge
+    '#6B5B95',  # Violet
+    '#88B04B',  # Vert
+    '#F7CAC9',  # Rose
+    '#92A8D1',  # Bleu clair
+    '#955251',  # Brun
+    '#B565A7',  # Violet clair
+    '#009B77',  # Vert clair
+    '#DAA520',  # Or
+    '#1E90FF',  # Bleu dodger
+    '#FFA500',  # Orange
+    '#800000'   # Rouge foncé
+]
+
+
+variables_qualitatives = [
+    "waterfront",
+    "view",
+    "condition",
+    "grade",
+]
+
+
+colors_qualitative = [
+    'midnightblue',
+    'orange',
+    'green',
+    'purple'
+]
